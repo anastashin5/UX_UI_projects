879081c188b320891dea47c53625be91962f1142
anastashin5 <anastashin5@gmail.com>
2023-01-23 21:24:06 -0500

    Coding

--- js/index.js
+++ js/index.js
@@ -45,3 +45,37 @@ function mouseOut() {
   document.getElementById("logo_img").style.width = "75px";
   document.getElementById("logo_img").style.height = "85px";
 }
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
+});
+
+
+
+/*Up button to scroll the site page to the top*/
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    this.el.classList.remove('btn-up_hide');
+  },
+  hide() {
+    this.el.classList.add('btn-up_hide');
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    document.querySelector('.btn-up').onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
